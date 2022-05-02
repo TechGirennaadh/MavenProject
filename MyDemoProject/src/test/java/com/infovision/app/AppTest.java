3b5e8ba104ecbceca021899edd52eb86474f7bd5
girennaadh <girinath009@gmail.com>
2022-05-02 16:33:51 +0530

    Maven project code commit

---
+++ MyDemoProject/src/test/java/com/infovision/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.infovision.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
